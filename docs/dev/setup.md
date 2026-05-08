01b4b4d151445b940733998c9bb8e2df3fbe0ca3
oswin902m <1665434994@qq.com>
2026-05-08 15:46:01 +0800

    setup vite-alias, oxfmt

---
+++ docs/dev/setup.md
@@ -0,0 +1,19 @@
+# Toolchain
+
+## Vite Alias
+
+## Oxfmt
+
+- https://oxc.rs/docs/guide/usage/formatter/quickstart.html
+
+# Router
+
+## Tanstack Router
+
+- https://tanstack.com/router/latest/docs/installation/manual
+
+# UI
+
+## Tailwind CSS
+
+## Daisy UI
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from '@rolldown/plugin-babel';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
